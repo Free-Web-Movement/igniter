8300b00088fbb752cc8f208b14b6323313aeec57
calidion <calidion@gmail.com>
2023-06-07 14:53:34 +0800

    init add server page

--- app/src/main/java/io/github/freewebmovement/igniter/servers/activity/ServerListActivity.java
+++ app/src/main/java/io/github/freewebmovement/igniter/servers/activity/ServerListActivity.java
@@ -1,9 +1,14 @@
 package io.github.freewebmovement.igniter.servers.activity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import io.github.freewebmovement.igniter.IgniterApplication;
 import io.github.freewebmovement.igniter.R;
@@ -30,5 +35,15 @@ public class ServerListActivity extends AppCompatActivity {
         fm.beginTransaction()
                 .replace(R.id.parent_fl, fragment, ServerListFragment.TAG)
                 .commitAllowingStateLoss();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        Context context = getApplicationContext();
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ServerListActivity.this, AddServerActivity.class);
+                ServerListActivity.this.startActivity(intent);
+            }
+        });
     }
 }
