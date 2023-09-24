42214a406e6f35f6406041e6bcf040a94383e162
calidion <calidion@gmail.com>
2023-09-24 18:36:02 +0800

    fixed url error

--- app/src/main/java/io/github/freewebmovement/igniter/constants/API.java
+++ app/src/main/java/io/github/freewebmovement/igniter/constants/API.java
@@ -3,7 +3,7 @@ package io.github.freewebmovement.igniter.constants;
 public class API {
     public static String SERVER_LIST_URI = "https://free-web-movement.github.io/assets/servers.json";
     public static String API_DOMAIN = "games.yikuaijiasu.top";
-    public static String BASE_URL = "https://$API_DOMAIN/";
+    public static String BASE_URL = "https://" + API_DOMAIN + "/";
     public static String API_QUOTA_PATH = "user/quota";
     public static String API_URI = "https://$API_DOMAIN:$API_PORT/";
     public static String API_QUOTA_KEY_USERNAME = "username";
